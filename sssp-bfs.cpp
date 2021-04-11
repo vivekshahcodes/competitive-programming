30d283140047a3547c1be8b93aa5bf24a99a3df7
Vivek Shah <vivekshahcodes@gmail.com>
2021-04-11 14:15:38 +0530

    Create sssp-bfs.cpp

---
+++ sssp-bfs.cpp
@@ -0,0 +1,26 @@
+void bfs(int s){
+
+    visited[s] = true;
+    distance[s] = 0;
+    q.push(s);
+
+    while(!q.empty()){
+
+        int x = q.front();
+        q.pop();
+
+        //Process the node
+
+        for(auto i:adj[x]){
+            
+            if(visited[i]){
+                continue;
+            }
+
+            visited[i] = true;
+            distance[i] = distance[x]+1;
+            q.push(i);
+
+        }
+    }
+}
