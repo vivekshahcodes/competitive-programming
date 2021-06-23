ed796f23368d2adae12e7afc97f9fa25bbaf03b4
Vivek Shah <vivekshahcodes@gmail.com>
2021-06-23 11:54:19 +0530

    Create A. Anton and Danik.cpp

---
+++ Codeforces/Codeforces Round #379 (Div. 2)/A. Anton and Danik.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+
+    int n;
+
+    cin>>n;
+
+    string s;
+
+    cin>>s;
+
+    int a=0,d=0;
+
+    for(int i=0;i<n;i++){
+        if(s[i]=='A'){
+            a++;
+        }else{
+            d++;
+        }
+    }
+
+    if(a>d){
+        cout<<"Anton";
+    }else if(a<d){
+        cout<<"Danik";
+    }else{
+        cout<<"Friendship";
+    }
+
+
+    return 0;
+}
