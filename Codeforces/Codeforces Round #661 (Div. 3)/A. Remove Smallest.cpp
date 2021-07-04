2d497f949972aed66739cce407ddb003d2c1c50d
Vivek Shah <vivekshahcodes@gmail.com>
2021-07-04 13:45:31 +0530

    Create A. Remove Smallest.cpp

---
+++ Codeforces/Codeforces Round #661 (Div. 3)/A. Remove Smallest.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+
+  int t;
+
+  cin>>t;
+
+  while(t--){
+
+    int n;
+
+    cin>>n;
+
+    int a[n];
+    
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+
+    sort(a,a+n);
+    
+    bool flag = true;
+
+    for(int i=1;i<n;i++){
+        if(a[i]-a[i-1]>=2){
+            flag = false;
+            break;
+        }
+    }
+
+    if(flag){
+        cout<<"YES"<<endl;
+    }else{
+        cout<<"NO"<<endl;
+    }
+
+  }
+
+return 0;
+
+}
