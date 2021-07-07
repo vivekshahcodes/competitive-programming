07664b000e8b1ea2a06f2f97f29a3a1bd9817657
Vivek Shah <vivekshahcodes@gmail.com>
2021-07-07 18:34:06 +0530

    Create A. Mishka and Game.cpp

---
+++ Codeforces/Codeforces Round #365 (Div. 2)/A. Mishka and Game.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+
+    int n;
+
+    cin>>n;
+
+    int a=0,b=0;
+
+    for(int i=0;i<n;i++){
+        
+        int x,y;
+
+        cin>>x>>y;
+
+        if(x>y){
+            a++;
+        }else if(x<y){
+            b++;
+        }
+
+    }
+
+    if(a>b){
+        cout<<"Mishka";
+    }else if(a<b){
+        cout<<"Chris";
+    }else{
+        cout<<"Friendship is magic!^^";
+    }
+
+    return 0;
+}
