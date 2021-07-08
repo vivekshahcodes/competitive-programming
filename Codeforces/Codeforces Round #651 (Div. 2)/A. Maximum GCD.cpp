32832a4a3b81b6f88b2d6616e5af11737a7ed309
Vivek Shah <vivekshahcodes@gmail.com>
2021-07-08 19:45:42 +0530

    Create A. Maximum GCD.cpp

---
+++ Codeforces/Codeforces Round #651 (Div. 2)/A. Maximum GCD.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+
+  int t;
+
+  cin>>t;
+
+  while(t--){
+
+    int n;
+
+    cin>>n;
+
+    cout<<n/2<<endl;
+
+  }
+
+  return 0;
+
+}
