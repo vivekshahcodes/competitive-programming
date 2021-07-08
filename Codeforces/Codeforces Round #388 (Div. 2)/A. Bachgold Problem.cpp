f2c9cc4360ee70916ed16cd695197dc42e7a708c
Vivek Shah <vivekshahcodes@gmail.com>
2021-07-08 19:31:51 +0530

    Create A. Bachgold Problem.cpp

---
+++ Codeforces/Codeforces Round #388 (Div. 2)/A. Bachgold Problem.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+
+    int n;
+
+    cin>>n;
+
+    cout<<n/2<<endl;
+
+    if(n&1){
+        
+        for(int i=1;i<=(n/2)-1;i++){
+            cout<<2<<" ";
+        }
+
+        cout<<3;
+
+    }else{
+
+        for(int i=1;i<=n/2;i++){
+            cout<<2<<" ";
+        }
+
+    }
+
+    return 0;
+}
