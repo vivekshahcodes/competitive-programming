536ebc1ed7dc80495581680aa207e0f653369ce9
Vivek Shah <vivekshahcodes@gmail.com>
2021-07-16 11:11:15 +0530

    Create A. FashionabLee.cpp

---
+++ Codeforces/Codeforces Round #652 (Div. 2)/A. FashionabLee.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+
+  int t;
+
+  cin>>t;
+
+  while(t--){
+
+    int n;
+
+    cin>>n;
+
+    if(n%4==0){
+        cout<<"YES"<<endl;
+    }else{
+        cout<<"NO"<<endl;
+    }
+
+  }
+
+  return 0;
+
+}
