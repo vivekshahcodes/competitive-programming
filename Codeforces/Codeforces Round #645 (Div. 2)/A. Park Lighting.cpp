f4d1d520daa674031f69a6c8fa53308adee6aad9
Vivek Shah <vivekshahcodes@gmail.com>
2021-07-16 11:05:49 +0530

    Create A. Park Lighting.cpp

---
+++ Codeforces/Codeforces Round #645 (Div. 2)/A. Park Lighting.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+
+  int t;
+
+  cin>>t;
+
+  while(t--){
+
+    int a,b;
+
+    cin>>a>>b;
+
+    cout<<((a*b)+1)/2<<endl;
+
+  }
+
+  return 0;
+
+}
