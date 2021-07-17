60f681844ebefb08b7b6cb1ce744f0435184ded8
Vivek Shah <vivekshahcodes@gmail.com>
2021-07-18 01:20:26 +0530

    Create A. Minutes Before the New Year.cpp

---
+++ Codeforces/Codeforces Round #611 (Div. 3)/A. Minutes Before the New Year.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+
+  int t;
+
+  cin>>t;
+
+  while(t--){
+
+    int n,k;
+
+    cin>>n>>k;
+
+    cout<<(23-n)*60+(60-k)<<endl;
+
+  }
+
+return 0;
+
+}
