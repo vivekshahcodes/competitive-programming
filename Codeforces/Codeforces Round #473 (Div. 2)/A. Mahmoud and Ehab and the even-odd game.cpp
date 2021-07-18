316ad7d18e0addbb2140793e3f6acfe4d69f178f
Vivek Shah <vivekshahcodes@gmail.com>
2021-07-18 18:19:58 +0530

    Create A. Mahmoud and Ehab and the even-odd game.cpp

---
+++ Codeforces/Codeforces Round #473 (Div. 2)/A. Mahmoud and Ehab and the even-odd game.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+
+    int n;
+
+    cin>>n;
+
+    if(n&1){
+        cout<<"Ehab";
+    }else{
+        cout<<"Mahmoud";
+    }
+
+    return 0;
+}
