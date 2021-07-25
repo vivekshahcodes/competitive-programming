dbcbfc2dd28ff0f77b2d4b5b73471f85926082da
Vivek Shah <vivekshahcodes@gmail.com>
2021-07-25 13:31:54 +0530

    Create Cyclic Quadrilateral.cpp

---
+++ Codechef/June CodeChef Starters 2021 (Rated for Div 3)/Cyclic Quadrilateral.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+
+  int t;
+
+  cin>>t;
+
+  while(t--){
+
+    int a,b,c,d;
+
+    cin>>a>>b>>c>>d;
+
+    if(a+c==180){
+        cout<<"YES"<<endl;
+    }else{
+        cout<<"NO"<<endl;
+    }
+
+
+  }
+
+  return 0;
+
+}
