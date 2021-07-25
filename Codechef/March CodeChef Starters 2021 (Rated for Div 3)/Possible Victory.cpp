0f707ec3c892d813bbe66d3c3b266ad27d6ea2d8
Vivek Shah <vivekshahcodes@gmail.com>
2021-07-25 13:11:15 +0530

    Create Possible Victory.cpp

---
+++ Codechef/March CodeChef Starters 2021 (Rated for Div 3)/Possible Victory.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+
+    int a,b,c;
+
+    cin>>a>>b>>c;
+
+    if((20-b)*36+c>a){
+        cout<<"YES"<<endl;
+    }else{
+        cout<<"NO"<<endl;
+    }
+
+    return 0;
+}
