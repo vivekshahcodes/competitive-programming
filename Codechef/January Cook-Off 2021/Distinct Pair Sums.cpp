ad1222b10c3ce5937eddc0a5bd5fc6579c687756
Vivek Shah <vivekshahcodes@gmail.com>
2021-07-28 16:34:50 +0530

    Create Distinct Pair Sums.cpp

---
+++ Codechef/January Cook-Off 2021/Distinct Pair Sums.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+
+  int t;
+
+  cin>>t;
+
+  while(t--){
+
+    ll a,b;
+
+    cin>>a>>b;
+
+    cout<<(2*b-2*a)+1<<endl;
+
+  }
+
+  return 0;
+
+}
