3121dd9d09215fa8e6e4ad44ef18ec4ea0cc8e49
Vivek Shah <vivekshahcodes@gmail.com>
2021-07-30 14:01:40 +0530

    Create TEST - Life, the Universe, and Everything.cpp

---
+++ SPOJ/TEST - Life, the Universe, and Everything.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n;
+
+    bool flag = true;
+
+    while(cin>>n){
+        if(n==42){
+            flag = false;
+        }
+        if(flag==true){
+            cout<<n<<endl;
+        }
+    }
+
+    return 0;
+}
