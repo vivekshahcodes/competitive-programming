a6595a9cfde21f12337c0ce3d7e86cca5a67d601
Vivek Shah <vivekshahcodes@gmail.com>
2021-07-31 10:40:01 +0530

    Create Pair Me.cpp

---
+++ Codechef/January Lunchtime 2021 (Unrated)/Pair Me.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+
+  int t;
+
+  cin>>t;
+
+  while(t--){
+
+    int a,b,c;
+
+    cin>>a>>b>>c;
+
+    if(a+b==c || b+c==a || a+c==b){
+        cout<<"YES"<<endl;
+    }else{
+        cout<<"NO"<<endl;
+    }
+
+  }
+
+  return 0;
+
+}
