e2a1a56951bff548d13f7e17814b1a8e762d9d10
Vivek Shah <vivekshahcodes@gmail.com>
2021-09-03 13:40:22 +0530

    Create A. Word Capitalization.cpp

---
+++ Codeforces/Codeforces Round #172 (Div. 2)/A. Word Capitalization.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+
+    string s;
+
+    cin>>s;
+
+    s[0] = toupper(s[0]);
+
+    cout<<s;
+
+    return 0;
+}
