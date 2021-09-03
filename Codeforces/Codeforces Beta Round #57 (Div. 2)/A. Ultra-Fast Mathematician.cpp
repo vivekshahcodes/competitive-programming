ee2f148b3fd8d8b0fe743fa76141c49774c4b988
Vivek Shah <vivekshahcodes@gmail.com>
2021-09-04 00:31:49 +0530

    Create A. Ultra-Fast Mathematician.cpp

---
+++ Codeforces/Codeforces Beta Round #57 (Div. 2)/A. Ultra-Fast Mathematician.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+
+    string a,b;
+
+    cin>>a>>b;
+
+    for(int i=0;i<a.length();i++){
+        if(a[i]==b[i]){
+            cout<<0;
+        }else{
+            cout<<1;
+        }
+    }
+
+    return 0;
+}
