032f8458e84e77a9fa2ac8a6e6f3c462b116b5e3
Vivek Shah <vivekshahcodes@gmail.com>
2021-09-03 23:27:50 +0530

    Create Primality Test.cpp

---
+++ Codechef/Miscellaneous/Primality Test.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+bool isPrime(int n){
+    
+    if(n==1){
+        return false;
+    }
+
+    for(int i=2;i*i<=n;i++){
+        if(n%i==0){
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main(){
+
+  int t;
+
+  cin>>t;
+
+  while(t--){
+
+    int n;
+
+    cin>>n;
+
+    if(isPrime(n)){
+        cout<<"yes"<<endl;
+    }else{
+        cout<<"no"<<endl;
+    }
+
+  }
+
+  return 0;
+
+}
