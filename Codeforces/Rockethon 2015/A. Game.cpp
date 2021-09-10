5aa81a14951ad89f316e37eec07fa05ada8b8a00
Vivek Shah <vivekshahcodes@gmail.com>
2021-09-11 01:37:39 +0530

    Create A. Game.cpp

---
+++ Codeforces/Rockethon 2015/A. Game.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+
+    int a,b,c,d;
+
+    cin>>a>>b>>c>>d;
+
+    if(a>b){
+        cout<<"First";
+    }else{
+        cout<<"Second";
+    }
+
+    return 0;
+}
