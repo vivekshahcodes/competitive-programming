17ce95515a957303e2e0e7b85beecc55e62a1ac4
Vivek Shah <vivekshahcodes@gmail.com>
2021-09-11 10:19:30 +0530

    Create A. Vladik and Courtesy.cpp

---
+++ Codeforces/Codeforces Round #416 (Div. 2)/A. Vladik and Courtesy.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+
+    int a,b;
+
+    cin>>a>>b;
+
+    int curr = 1;
+    bool vladik = true;
+
+    while(true){
+        if(vladik){
+            if(a-curr>=0){
+                a-=curr;
+                vladik = false;
+                curr++;
+            }else{
+                break;
+            }
+        }else{
+            if(b-curr>=0){
+                b-=curr;
+                vladik = true;
+                curr++;
+            }else{
+                break;
+            }
+        }
+    }
+
+    if(vladik){
+        cout<<"Vladik";
+    }else{
+        cout<<"Valera";
+    }
+
+    return 0;
+}
