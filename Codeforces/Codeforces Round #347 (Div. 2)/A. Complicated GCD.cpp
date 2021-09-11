dcd19dd41310b6e22f7abe08f47e1b81ba8701c7
Vivek Shah <vivekshahcodes@gmail.com>
2021-09-11 09:16:08 +0530

    Create A. Complicated GCD.cpp

---
+++ Codeforces/Codeforces Round #347 (Div. 2)/A. Complicated GCD.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+
+    string s,t;
+
+    cin>>s>>t;
+
+    if(s==t){
+        cout<<s;
+    }else{
+        cout<<1;
+    }
+
+    return 0;
+}
