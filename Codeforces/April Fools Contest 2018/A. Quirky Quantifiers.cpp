b7d5422546c746d0c0d885d67bcc1178cd1f34af
Vivek Shah <vivekshahcodes@gmail.com>
2021-09-11 22:05:22 +0530

    Create A. Quirky Quantifiers.cpp

---
+++ Codeforces/April Fools Contest 2018/A. Quirky Quantifiers.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+
+    int n;
+
+    cin>>n;
+
+    if(n&1){
+        cout<<1;
+    }else{
+        cout<<0;
+    }
+
+    return 0;
+}
