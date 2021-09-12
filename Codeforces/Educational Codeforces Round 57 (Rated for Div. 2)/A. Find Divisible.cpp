36997d4d7c9d1eb630ede67f190c13f55eb6b9ca
Vivek Shah <vivekshahcodes@gmail.com>
2021-09-12 13:04:54 +0530

    Create A. Find Divisible.cpp

---
+++ Codeforces/Educational Codeforces Round 57 (Rated for Div. 2)/A. Find Divisible.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+
+    cin>>t;
+
+    while(t--){
+
+        int l,r;
+
+        cin>>l>>r;
+
+        cout<<l<<" "<<l*2<<endl;
+    }
+
+    return 0;
+}
