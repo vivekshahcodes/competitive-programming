d4cec374c1c9c00a8d08a901e38ab975f7560763
Vivek Shah <vivekshahcodes@gmail.com>
2021-09-12 18:23:31 +0530

    Create A. Two Rabbits.cpp

---
+++ Codeforces/Codeforces Round #620 (Div. 2)/A. Two Rabbits.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+
+    cin>>t;
+
+    while(t--){
+
+        int x,y,a,b;
+
+        cin>>x>>y>>a>>b;
+
+        if((y-x)%(a+b)==0){
+            cout<<(y-x)/(a+b)<<endl;
+        }else{
+            cout<<-1<<endl;
+        }
+    }
+
+    return 0;
+}
