0bec86853bd671ad32328d07225701d0cd63c3c3
Vivek Shah <vivekshahcodes@gmail.com>
2021-09-12 13:09:57 +0530

    Create A. Integer Sequence Dividing.cpp

---
+++ Codeforces/Codeforces Round #531 (Div. 3)/A. Integer Sequence Dividing.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+
+    cin>>n;
+
+    if(n%4==1||n%4==2){
+        cout<<1;
+    }else{
+        cout<<0;
+    }
+
+    return 0;
+}
