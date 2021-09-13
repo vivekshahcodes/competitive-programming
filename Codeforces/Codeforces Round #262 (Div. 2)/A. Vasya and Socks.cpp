e5eb969044af08bc1027b8234da346c94963f79a
Vivek Shah <vivekshahcodes@gmail.com>
2021-09-13 23:46:10 +0530

    Create A. Vasya and Socks.cpp

---
+++ Codeforces/Codeforces Round #262 (Div. 2)/A. Vasya and Socks.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int a,b;
+
+    cin>>a>>b;
+
+    cout<<a+(a-1)/(b-1);
+
+    return 0;
+}
