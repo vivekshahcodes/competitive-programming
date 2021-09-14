0b2d23c60ce9db796365d5d21185ed765ef9d7d3
Vivek Shah <vivekshahcodes@gmail.com>
2021-09-14 17:33:44 +0530

    Create A. Even Odds.cpp

---
+++ Codeforces/Codeforces Round #188 (Div. 2)/A. Even Odds.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll n,k;
+
+    cin>>n>>k;
+
+    if(k<=(n+1)/2){
+        cout<<(2*k)-1;
+    }else{
+        k-=(n+1)/2;
+        cout<<2*k;
+    }
+
+    return 0;
+}
