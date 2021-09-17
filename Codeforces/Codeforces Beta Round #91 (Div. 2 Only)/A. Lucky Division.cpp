e3dd1ab33176f4a1c54e4d4bd6094d7ddc613a49
Vivek Shah <vivekshahcodes@gmail.com>
2021-09-17 17:10:35 +0530

    Create A. Lucky Division.cpp

---
+++ Codeforces/Codeforces Beta Round #91 (Div. 2 Only)/A. Lucky Division.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+bool isLucky(int n){
+
+    int x = n;
+
+    while(x>0){
+
+        if((x%10)!=4 && (x%10)!=7){
+            return false;
+        }
+
+        x/=10;
+    }
+
+    return true;
+}
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+
+    cin>>n;
+
+    vector<int> luckyNums;
+
+    for(int i=1;i<=1000;i++){
+        if(isLucky(i)){
+            luckyNums.push_back(i);
+        }
+    }
+
+    bool flag = false;
+
+    for(int i:luckyNums){
+        if(n%i==0){
+            flag = true;
+            break;
+        }
+    }
+
+    if(flag){
+        cout<<"YES";
+    }else{
+        cout<<"NO";
+    }
+
+    return 0;
+}
