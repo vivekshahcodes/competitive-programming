744280c41daaf74a0bbd262c3bb063bdefbb2ff4
Vivek Shah <vivekshahcodes@gmail.com>
2021-09-21 03:22:57 +0530

    Create A. Fake NP.cpp

---
+++ Codeforces/Codeforces Round #411 (Div. 2)/A. Fake NP.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    string s,t;
+
+    cin>>s>>t;
+
+    if(s==t){
+        cout<<s;
+    }else{
+        cout<<2;
+    }
+
+    return 0;
+}
