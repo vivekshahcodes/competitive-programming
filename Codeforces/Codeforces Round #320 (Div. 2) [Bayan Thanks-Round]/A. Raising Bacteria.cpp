321e52e49e987fbc66b615c7c3f176c94d0307c5
Vivek Shah <vivekshahcodes@gmail.com>
2021-09-21 00:23:10 +0530

    Create A. Raising Bacteria.cpp

---
+++ Codeforces/Codeforces Round #320 (Div. 2) [Bayan Thanks-Round]/A. Raising Bacteria.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll n;
+
+    cin>>n;
+
+    cout<<__builtin_popcountll(n);
+
+    return 0;
+}
