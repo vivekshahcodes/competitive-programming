8bac0289f12741768e1210e127d46b8db646dd89
Vivek Shah <vivekshahcodes@gmail.com>
2021-10-01 00:55:11 +0530

    Create Find Second Largest.cpp

---
+++ Codechef/DSA Learning Series/Easy Problems to Get Started/Find Second Largest.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    vector<int> v;
+
+    for(int i=0;i<3;i++){
+        int input;
+        cin>>input;
+        v.push_back(input);
+    }
+    
+    sort(v.begin(),v.end());
+
+    cout<<v[1];
+
+    return 0;
+}
