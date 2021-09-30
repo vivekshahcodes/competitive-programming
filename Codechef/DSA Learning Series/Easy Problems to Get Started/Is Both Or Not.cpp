2bc7354aadcb27bd414e3cb842bfb1cdfc9cd351
Vivek Shah <vivekshahcodes@gmail.com>
2021-10-01 00:49:31 +0530

    Create Is Both Or Not.cpp

---
+++ Codechef/DSA Learning Series/Easy Problems to Get Started/Is Both Or Not.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+
+    int n;
+
+    cin>>n;
+
+    if(n%5==0 && n%11==0){
+        cout<<"BOTH";
+    }else if(n%5==0 || n%11==0){
+        cout<<"ONE";
+    }else{
+        cout<<"NONE";
+    }
+
+    return 0;
+}
