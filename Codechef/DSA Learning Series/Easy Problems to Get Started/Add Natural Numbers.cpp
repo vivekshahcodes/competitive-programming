da5c8a2dd80831c4e4dd605468c9b3032e3b92d6
Vivek Shah <vivekshahcodes@gmail.com>
2021-10-01 01:10:15 +0530

    Create Add Natural Numbers.cpp

---
+++ Codechef/DSA Learning Series/Easy Problems to Get Started/Add Natural Numbers.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll n;
+
+    cin>>n;
+
+    cout<<(n*(n+1))/2;
+
+    return 0;
+}
