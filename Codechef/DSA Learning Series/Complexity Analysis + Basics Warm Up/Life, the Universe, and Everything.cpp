0b0bcec4eba252a627c14aceceb4645b9609bca4
Vivek Shah <vivekshahcodes@gmail.com>
2021-10-01 09:14:48 +0530

    Create Life, the Universe, and Everything.cpp

---
+++ Codechef/DSA Learning Series/Complexity Analysis + Basics Warm Up/Life, the Universe, and Everything.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+
+    while(cin>>n){
+        if(n==42){
+            break;
+        }else{
+            cout<<n<<endl;
+        }
+    }
+
+    return 0;
+}
