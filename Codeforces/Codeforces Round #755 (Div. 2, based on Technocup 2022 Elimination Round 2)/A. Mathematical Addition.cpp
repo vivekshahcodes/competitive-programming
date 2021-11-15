d68a7ea54c967ef215b0ab14e8b510f0741f1028
Vivek Shah <vivekshahcodes@gmail.com>
2021-11-16 03:46:59 +0530

    Create A. Mathematical Addition.cpp

---
+++ Codeforces/Codeforces Round #755 (Div. 2, based on Technocup 2022 Elimination Round 2)/A. Mathematical Addition.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+
+    cin>>t;
+
+    while(t--){
+
+        ll a,b;
+
+        cin>>a>>b;
+
+        cout<<-1*a*a<<" "<<b*b<<endl;
+    }
+
+    return 0;
+}
