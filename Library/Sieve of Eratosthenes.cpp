37d7bfb6cc2748215c7f9182297c92d2e15a18b2
Vivek Shah <vivekshahcodes@gmail.com>
2021-11-26 12:13:13 +0530

    Update Sieve of Eratosthenes.cpp

--- Library/Sieve of Eratosthenes.cpp
+++ Library/Sieve of Eratosthenes.cpp
@@ -1,19 +1,19 @@
-ll p[1000005] = {0};
+bool p[90000005] = {false};
 
 void sieve(){
 
-    for(ll i=3;i<=1000000;i+=2){
-        p[i] = 1;
+    for(ll i=3;i<=90000000;i+=2){
+        p[i] = true;
     }
 
-    for(ll i=3;i*i<=1000000;i+=2){
-        if(p[i]==1){
-            for(ll j=i*i;j<=1000000;j+=i){
-                p[j] = 0;
+    for(ll i=3;i*i<=90000000;i+=2){
+        if(p[i]==true){
+            for(ll j=i*i;j<=90000000;j+=i){
+                p[j] = false;
             }
         }
     }
 
-    p[2] = 1;
-    p[0] = p[1] = 0;
+    p[2] = true;
+    p[0] = p[1] = false;
 }
