1ac2238ea489f5a60a503f00b237e285b84c4ba8
Vivek Shah <vivekshahcodes@gmail.com>
2021-12-10 15:17:13 +0530

    Create C. Oh Those Palindromes.cpp

---
+++ Codeforces/Codeforces Round #516 (Div. 2, by Moscow Team Olympiad)/C. Oh Those Palindromes.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+
+    cin>>n;
+
+    string s;
+
+    cin>>s;
+
+    sort(s.begin(),s.end());
+
+    cout<<s;
+
+    return 0;
+}
