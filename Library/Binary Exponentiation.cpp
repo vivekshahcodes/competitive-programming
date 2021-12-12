3cd0df495e964d0d24dc05c93cdc8635fffbf8e7
Vivek Shah <vivekshahcodes@gmail.com>
2021-12-12 20:33:20 +0530

    Create Binary Exponentiation.cpp

---
+++ Library/Binary Exponentiation.cpp
@@ -0,0 +1,12 @@
+int binpow(int a, int b) {
+    int res = 1;
+    while (b>0){
+
+        if (b&1){
+            res = res * a;
+        }    
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
