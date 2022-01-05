cee127dc46d9384375c15e90672a702ff748456f
Vivek Shah <vivekshahcodes@gmail.com>
2022-01-05 12:42:10 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,22 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define endl "\n"
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x<<" "; _print(x); cerr<< "\n";
+#else
+#define debug(x)
+#endif
+
+template<class T> void _print(T a) { cerr << a; }
+template<class T> void _print(vector<T> v) { cerr << "[ "; for (T i : v) { _print(i); cerr << " "; } cerr << "]"; }
+template<class T> void _print(vector<vector<T>> v) { cerr << "\n"; for (auto i : v) { cerr << "[ "; for (T j : i) { cerr << j << " "; } cerr << "]\n"; } }
+template<class T> void _print(set<T> s) { cerr << "[ "; for (T i : s) { _print(i); cerr << " "; } cerr << "]"; }
+template<class T, class V> void _print(map<T, V> m) { cerr << "\n"; for (auto i : m) { _print(i.first); cerr << " "; _print(i.second); cerr << "\n"; } }
+
 typedef long long ll;
 
-int main(){
+int main() {
 
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	freopen("error.txt", "w", stderr);
+#endif
 
-    int t;
+	fastio();
 
-    cin>>t;
+	int t;
+	cin>>t;
 
-    while(t--){
+	while(t--){
 
-        
-    }
+		
 
-    return 0;
+	}
+
+	return 0;
 }
