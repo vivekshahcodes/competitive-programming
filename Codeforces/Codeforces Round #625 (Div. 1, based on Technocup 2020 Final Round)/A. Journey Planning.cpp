1c6fef9dcdb3472f9df46a350dfaf7968b889221
Vivek Shah <vivekshahcodes@gmail.com>
2022-01-10 16:53:09 +0530

    Create A. Journey Planning.cpp

---
+++ Codeforces/Codeforces Round #625 (Div. 1, based on Technocup 2020 Final Round)/A. Journey Planning.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define endl "\n"
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x<<" "; _print(x); cerr<< "\n";
+#else
+#define debug(x)
+#endif
+
+template<class T> void _print(T a) { cerr << a; }
+template<class T> void _print(vector<T> v) { cerr << "[ "; for (T i : v) { _print(i); cerr << " "; } cerr << "]"; }
+template<class T> void _print(vector<vector<T>> v) { cerr << "\n"; for (auto i : v) { cerr << "[ "; for (T j : i) { cerr << j << " "; } cerr << "]\n"; } }
+template<class T> void _print(set<T> s) { cerr << "[ "; for (T i : s) { _print(i); cerr << " "; } cerr << "]"; }
+template<class T, class V> void _print(map<T, V> m) { cerr << "\n"; for (auto i : m) { _print(i.first); cerr << " "; _print(i.second); cerr << "\n"; } }
+
+typedef long long ll;
+
+int main() {
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	freopen("error.txt", "w", stderr);
+#endif
+
+	fastio();
+
+	ll t = 1;
+
+	while(t--){
+
+		ll n;
+		cin>>n;
+
+		map<ll,ll> m;
+
+		for(ll i=0;i<n;i++){
+			ll input;
+			cin>>input;
+			m[i-input]+=input;
+		}
+
+		ll ans = 0;
+
+		for(auto i:m){
+			ans = max(ans,i.second);
+		}
+
+		cout<<ans;
+
+	}
+
+	return 0;
+}
