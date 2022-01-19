61fee5873e10b1990c806c7fb1375f7b82576185
Vivek Shah <vivekshahcodes@gmail.com>
2022-01-19 12:20:46 +0530

    Create C. Compression and Expansion.cpp

---
+++ Codeforces/Deltix Round, Spring 2021 (open for everyone, rated, Div. 1 + Div. 2)/C. Compression and Expansion.cpp
@@ -0,0 +1,78 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define endl "\n"
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x<<" "; _print(x); cerr<< "\n";
+#else
+#define debug(x)
+#endif
+
+template<class T> void _print(T a) { cerr << a; }
+template<class T, class V> void _print(pair<T,V> p){ _print('{'); _print(p.first); _print(','); _print(p.second); _print('}'); }
+template<class T> void _print(vector<T> v) { cerr << "[ "; for (T i : v) { _print(i); cerr << " "; } cerr << "]"; }
+template<class T> void _print(vector<vector<T>> v) { cerr << "\n"; for (auto i : v) { cerr << "[ "; _print(i); cerr << "]\n"; } }
+template<class T> void _print(set<T> s) { cerr << "[ "; for (T i : s) { _print(i); cerr << " "; } cerr << "]"; }
+template<class T, class V> void _print(map<T, V> m) { cerr << "\n"; for (auto i : m) { _print(i.first); cerr << " "; _print(i.second); cerr << "\n"; } }
+
+
+typedef long long ll;
+
+/*------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+int main() {
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	freopen("error.txt", "w", stderr);
+#endif
+
+	fastio();
+
+	int t;
+	cin>>t;
+
+	while(t--){
+
+		int n;
+		cin>>n;
+
+		vector<int> v;
+
+		for(int i=0;i<n;i++){
+			int input;
+			cin>>input;
+			if(input==1){
+				v.push_back(input);
+			}else{
+				while(v.size()>0 && v.back()+1!=input){
+					v.pop_back();
+				}
+
+				if(v.empty()){
+					v.push_back(input);
+				}else{
+					v.pop_back();
+					v.push_back(input);
+				}
+			}
+
+			for(int i=0;i<v.size();i++){
+				cout<<v[i];
+				if(i!=v.size()-1){
+					cout<<".";
+				}
+			}
+
+			cout<<endl;
+		}
+	}
+
+	return 0;
+}
