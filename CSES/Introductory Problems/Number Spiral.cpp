935e5025217961caafa5c9dcc924855920e4b0fb
Vivek Shah <vivekshahcodes@gmail.com>
2022-01-20 19:07:53 +0530

    Create Number Spiral.cpp

---
+++ CSES/Introductory Problems/Number Spiral.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define endl "\n"
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x<<" "; _print(x); cerr<< "\n";
+#else
+#define debug(x)
+#endif
+
+template<class T> void _print(T a) { cerr << a; }
+template<class T, class V> void _print(pair<T,V> p){ _print('{'); _print(p.first); _print(','); _print(p.second); _print('}'); }
+template<class T> void _print(vector<T> v) { cerr << "[ "; for (T i : v) { _print(i); cerr << " "; } cerr << "]"; }
+template<class T> void _print(vector<vector<T>> v) { cerr << "\n"; for (auto i : v) { cerr << "[ "; _print(i); cerr << "]\n"; } }
+template<class T> void _print(set<T> s) { cerr << "[ "; for (T i : s) { _print(i); cerr << " "; } cerr << "]"; }
+template<class T, class V> void _print(map<T, V> m) { cerr << "\n"; for (auto i : m) { _print(i.first); cerr << " "; _print(i.second); cerr << "\n"; } }
+
+
+typedef long long ll;
+
+/*------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+
+
+/*------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+int main() {
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	freopen("error.txt", "w", stderr);
+#endif
+
+	fastio();
+
+	int t;
+	cin>>t;
+
+	while(t--){
+
+		ll x,y;
+		cin>>x>>y;
+
+		ll m = max(x,y);
+		ll sq = m*m;
+
+		ll a,b;
+
+		if(sq&1){
+			a = 1, b = m;
+			cout<<sq-(abs(x-a)+abs(y-b));
+		}else{
+			a = m, b = 1;
+			cout<<sq-(abs(x-a)+abs(y-b));
+		}
+
+		cout<<endl;
+	}
+
+	return 0;
+}
