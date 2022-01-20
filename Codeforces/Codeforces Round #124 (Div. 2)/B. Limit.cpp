4b0d97c7fbc322d851e642e3211e55701572bb70
Vivek Shah <vivekshahcodes@gmail.com>
2022-01-20 21:08:23 +0530

    Create B. Limit.cpp

---
+++ Codeforces/Codeforces Round #124 (Div. 2)/B. Limit.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define endl "\n"
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x<<" "; _print(x); cerr<< "\n";
+#else
+#define debug(x)
+#endif
+
+template<class T> void _print(T a) { cerr << a; }
+template<class T, class V> void _print(pair<T,V> p){ _print('{'); _print(p.first); _print(','); _print(p.second); _print('}'); }
+template<class T> void _print(vector<T> v) { cerr << "[ "; for (T i : v) { _print(i); cerr << " "; } cerr << "]"; }
+template<class T> void _print(vector<vector<T>> v) { cerr << "\n"; for (auto i : v) { cerr << "[ "; _print(i); cerr << "]\n"; } }
+template<class T> void _print(set<T> s) { cerr << "[ "; for (T i : s) { _print(i); cerr << " "; } cerr << "]"; }
+template<class T, class V> void _print(map<T, V> m) { cerr << "\n"; for (auto i : m) { _print(i.first); cerr << " "; _print(i.second); cerr << "\n"; } }
+
+
+typedef long long ll;
+
+/*------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+
+
+/*------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+int main() {
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	freopen("error.txt", "w", stderr);
+#endif
+
+	fastio();
+
+	int t=1;
+
+	while(t--){
+
+		int n,m;
+		cin>>n>>m;
+
+		vector<int> v(n+1), w(m+1);
+
+		for(int i=0;i<n+1;i++){
+			cin>>v[i];
+		}
+
+		for(int i=0;i<m+1;i++){
+			cin>>w[i];
+		}
+
+		if(n<m){
+			cout<<"0/1";
+		}else if(n>m){
+			if(v[0]*w[0]<0){
+				cout<<"-Infinity";
+			}else{
+				cout<<"Infinity";
+			}
+		}else{
+			int x = v[0];
+			int y = w[0];
+
+			int g = __gcd(abs(x),abs(y));
+
+			if(x*y<0){
+				cout<<"-"<<abs(x)/g<<"/"<<abs(y)/g;
+			}else{
+				cout<<abs(x)/g<<"/"<<abs(y)/g;
+			}
+		}
+	}
+
+	return 0;
+}
