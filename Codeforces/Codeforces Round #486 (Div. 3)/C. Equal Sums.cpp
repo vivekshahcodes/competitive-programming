54e41f2751a50cb9e689ad573dfadc999d40bf17
Vivek Shah <vivekshahcodes@gmail.com>
2022-02-08 15:26:53 +0530

    Create C. Equal Sums.cpp

---
+++ Codeforces/Codeforces Round #486 (Div. 3)/C. Equal Sums.cpp
@@ -0,0 +1,94 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define endl "\n"
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x<<" "; _print(x); cerr<< "\n";
+#else
+#define debug(x)
+#endif
+
+template<class T> void _print(T a) { cerr << a; }
+template<class T, class V> void _print(pair<T,V> p){ _print('{'); _print(p.first); _print(','); _print(p.second); _print('}'); }
+template<class T> void _print(vector<T> v) { cerr << "[ "; for (T i : v) { _print(i); cerr << " "; } cerr << "]"; }
+template<class T> void _print(vector<vector<T>> v) { cerr << "\n"; for (auto i : v) { cerr << "[ "; _print(i); cerr << "]\n"; } }
+template<class T> void _print(set<T> s) { cerr << "[ "; for (T i : s) { _print(i); cerr << " "; } cerr << "]"; }
+template<class T, class V> void _print(map<T, V> m) { cerr << "\n"; for (auto i : m) { _print(i.first); cerr << " "; _print(i.second); cerr << "\n"; } }
+
+
+typedef long long ll;
+
+/*------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+
+
+/*------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+int main() {
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	freopen("error.txt", "w", stderr);
+#endif
+
+	fastio();
+
+	int t=1;
+
+	while(t--){
+
+		map<int,pair<int,int>> m;
+
+		int k;
+		cin>>k;
+
+		bool flag = false;
+
+		for(int j=0;j<k;j++){
+
+			int n;
+			cin>>n;
+
+			vector<int> v(n);
+
+			int sum = 0;
+			for(int i=0;i<n;i++){
+				cin>>v[i];
+				sum += v[i];
+			}
+
+			for(int i=0;i<n;i++){
+				int x = sum-v[i];
+				debug(x);
+				if(m.count(x)==0){
+					pair<int,int> p = {j+1,i+1};
+					m[x] = p;
+				}else if(m[x].first==j+1){
+					continue;
+				}else{
+					cout<<"YES"<<endl;
+					cout<<m[x].first<<" "<<m[x].second<<endl;
+					cout<<j+1<<" "<<i+1<<endl;
+					flag = true;
+					break;
+				}
+			}
+
+			if(flag){
+				break;
+			}
+		}
+
+		debug(m);
+
+		if(!flag){
+			cout<<"NO"<<endl;
+		}
+		
+	}
+
+	return 0;
+}
