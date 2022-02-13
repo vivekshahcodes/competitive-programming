6dc24215ad926552486dd5a75d90e5b307db936c
Vivek Shah <vivekshahcodes@gmail.com>
2022-02-13 14:00:36 +0530

    Create C. Everyone is a Winner!.cpp

---
+++ Codeforces/Codeforces Round #603 (Div. 2)/C. Everyone is a Winner!.cpp
@@ -0,0 +1,78 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define endl "\n"
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x<<" "; _print(x); cerr<< "\n";
+#else
+#define debug(x)
+#endif
+
+template<class T> void _print(T a) { cerr << a; }
+template<class T, class V> void _print(pair<T,V> p){ _print('{'); _print(p.first); _print(','); _print(p.second); _print('}'); }
+template<class T> void _print(vector<T> v) { cerr << "[ "; for (T i : v) { _print(i); cerr << " "; } cerr << "]"; }
+template<class T> void _print(vector<vector<T>> v) { cerr << "\n"; for (auto i : v) { cerr << "[ "; _print(i); cerr << "]\n"; } }
+template<class T> void _print(set<T> s) { cerr << "[ "; for (T i : s) { _print(i); cerr << " "; } cerr << "]"; }
+template<class T, class V> void _print(map<T, V> m) { cerr << "\n"; for (auto i : m) { _print(i.first); cerr << " "; _print(i.second); cerr << "\n"; } }
+
+
+typedef long long ll;
+
+/*------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+void factors(ll n, vector<ll>& f){
+
+	for(ll i=1;i*i<=n;i++){
+		ll x = n/i;
+		if(i*x<=n){
+			if(i==x){
+				f.push_back(i);
+				continue;
+			}
+			f.push_back(i);
+			f.push_back(x);
+		}
+	}
+}
+
+/*------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+int main() {
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	freopen("error.txt", "w", stderr);
+#endif
+
+	fastio();
+
+	int t;
+	cin>>t;
+
+	while(t--){
+
+		ll n;
+		cin>>n;
+
+		vector<ll> f;
+		
+		factors(n,f);
+
+		f.push_back(0);
+
+		sort(f.begin(),f.end());
+
+		cout<<f.size()<<endl;
+		for(ll i:f){
+			cout<<i<<" ";
+		}
+
+		cout<<endl;
+		
+	}
+
+	return 0;
+}
