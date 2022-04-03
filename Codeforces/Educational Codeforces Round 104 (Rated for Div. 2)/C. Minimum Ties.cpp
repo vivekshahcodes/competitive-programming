2e32c79f9aea569ca901a5db4126ef752d59ed02
Vivek Shah <vivekshahcodes@gmail.com>
2022-04-04 02:23:40 +0530

    Create C. Minimum Ties.cpp

---
+++ Codeforces/Educational Codeforces Round 104 (Rated for Div. 2)/C. Minimum Ties.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define endl "\n"
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x<<" "; _print(x); cerr<< "\n";
+#else
+#define debug(x)
+#endif
+
+template<class T> void _print(T a) { cerr << a; }
+template<class T, class V> void _print(pair<T,V> p){ _print('{'); _print(p.first); _print(','); _print(p.second); _print('}'); }
+template<class T> void _print(vector<T> v) { cerr << "[ "; for (T i : v) { _print(i); cerr << " "; } cerr << "]"; }
+template<class T> void _print(vector<vector<T>> v) { cerr << "\n"; for (auto i : v) { cerr << "[ "; _print(i); cerr << "]\n"; } }
+template<class T> void _print(set<T> s) { cerr << "[ "; for (T i : s) { _print(i); cerr << " "; } cerr << "]"; }
+template<class T, class V> void _print(map<T, V> m) { cerr << "\n"; for (auto i : m) { _print(i.first); cerr << " "; _print(i.second); cerr << "\n"; } }
+
+
+typedef long long ll;
+
+/*------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+
+
+/*------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+int main() {
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	freopen("error.txt", "w", stderr);
+#endif
+
+	fastio();
+
+	int t;
+	cin>>t;
+
+	while(t--){
+
+		int n;
+		cin>>n;
+
+		for(int i=1;i<n;i++){
+			for(int j=i+1;j<=n;j++){
+				int diff = j-i;
+				if(2*diff<n){
+					cout<<1<<" ";
+				}else if(2*diff==n){
+					cout<<0<<" ";
+				}else{
+					cout<<-1<<" ";
+				}
+			}
+		}
+
+		cout<<endl;
+	}
+
+	return 0;
+}
