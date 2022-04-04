a2e9ea1bde38f771c4c2fe9577a28fbec8781eae
Vivek Shah <vivekshahcodes@gmail.com>
2022-04-05 01:08:51 +0530

    Create E1. Close Tuples (easy version).cpp

---
+++ Codeforces/Codeforces Round #690 (Div. 3)/E1. Close Tuples (easy version).cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define endl "\n"
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x<<" "; _print(x); cerr<< "\n";
+#else
+#define debug(x)
+#endif
+
+template<class T> void _print(T a) { cerr << a; }
+template<class T, class V> void _print(pair<T,V> p){ _print('{'); _print(p.first); _print(','); _print(p.second); _print('}'); }
+template<class T> void _print(vector<T> v) { cerr << "[ "; for (T i : v) { _print(i); cerr << " "; } cerr << "]"; }
+template<class T> void _print(vector<vector<T>> v) { cerr << "\n"; for (auto i : v) { cerr << "[ "; _print(i); cerr << "]\n"; } }
+template<class T> void _print(set<T> s) { cerr << "[ "; for (T i : s) { _print(i); cerr << " "; } cerr << "]"; }
+template<class T, class V> void _print(map<T, V> m) { cerr << "\n"; for (auto i : m) { _print(i.first); cerr << " "; _print(i.second); cerr << "\n"; } }
+
+
+typedef long long ll;
+
+/*------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+
+
+/*------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+int main() {
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	freopen("error.txt", "w", stderr);
+#endif
+
+	fastio();
+
+	int t;
+	cin>>t;
+
+	while(t--){
+
+		ll n;
+		cin>>n;
+
+		vector<ll> v(n);
+		for(ll i=0;i<n;i++){
+			cin>>v[i];
+		}
+
+		sort(v.begin(),v.end());
+
+		ll i=0,j=0,ans=0;
+
+		while(i<n-2){
+
+			while(j<n && v[j]-v[i]<=2){
+				j++;
+			}
+
+			j--;
+
+			ll d = max(0ll,j-i-1);
+			ans += (d*(d+1))/2;
+
+			i++;
+		}
+
+		cout<<ans<<endl;
+
+		debug(v);
+	}
+
+	return 0;
+}
