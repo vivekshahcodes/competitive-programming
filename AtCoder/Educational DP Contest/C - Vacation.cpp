bf5d9d07fda48cba179ab839c169fdd05721a48a
Vivek Shah <vivekshahcodes@gmail.com>
2022-06-25 05:10:43 +0530

    Update C - Vacation.cpp

--- AtCoder/Educational DP Contest/C - Vacation.cpp
+++ AtCoder/Educational DP Contest/C - Vacation.cpp
@@ -1,83 +1,184 @@
+//Recursion
+//TC - O(3^n)
+//SC - O(n) for the stack space
+
 #include<bits/stdc++.h>
 using namespace std;
 
-#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
-#define endl "\n"
-
-#ifndef ONLINE_JUDGE
-#define debug(x) cerr << #x<<" "; _print(x); cerr<< "\n";
-#else
-#define debug(x)
-#endif
-
-template<class T> void _print(T a) { cerr << a; }
-template<class T, class V> void _print(pair<T,V> p){ _print('{'); _print(p.first); _print(','); _print(p.second); _print('}'); }
-template<class T> void _print(vector<T> v) { cerr << "[ "; for (T i : v) { _print(i); cerr << " "; } cerr << "]"; }
-template<class T> void _print(vector<vector<T>> v) { cerr << "\n"; for (auto i : v) { cerr << "[ "; _print(i); cerr << "]\n"; } }
-template<class T> void _print(set<T> s) { cerr << "[ "; for (T i : s) { _print(i); cerr << " "; } cerr << "]"; }
-template<class T, class V> void _print(map<T, V> m) { cerr << "\n"; for (auto i : m) { _print(i.first); cerr << " "; _print(i.second); cerr << "\n"; } }
-
-
-typedef long long ll;
-
-/*------------------------------------------------------------------------------------------------------------------------------------------------------*/
-
-int maxPoints(int day, int activity, vector<vector<int>>& a, vector<vector<int>>& dp){
-
-    if(day==0){
-
-        int mp = 0;
-
-        for(int i=0;i<3;i++){
-            if(activity!=i){
-                mp = max(mp,a[0][i]);
-            }
-        }
-
-        return dp[day][activity] = mp;
-    }
-
-    if(dp[day][activity]!=-1){
-        return dp[day][activity];
-    }
-
-    int mp = 0;
-
-    for(int i=0;i<3;i++){
-        if(activity!=i){
-            mp = max(mp,a[day][i]+maxPoints(day-1,i,a,dp));
-        }
-    }
-
-    return dp[day][activity] = mp;
+int maxPoints(int a, int n, vector<vector<int>>& v){
+	
+	if(n < 0){
+		return 0;
+	}
+	
+	int best = 0;
+	
+	for(int i = 0; i < 3; i ++){
+		if(i != a){
+			best = max(best, maxPoints(i, n - 1, v) + v[n][i]);
+		}
+	}
+	
+	return best;
 }
 
-/*------------------------------------------------------------------------------------------------------------------------------------------------------*/
+int main(){
+	
+	int n;
+	cin>>n;
+	
+	vector<vector<int>> v(n, vector<int> (3));
+	
+	for(int i = 0; i < n; i ++){
+		for(int j = 0; j < 3; j ++){
+			cin>>v[i][j];
+		}
+	}
+	
+	cout<<maxPoints(3, n - 1, v);
+	
+	return 0;
+}
 
-int main() {
 
-#ifndef ONLINE_JUDGE
-  freopen("input.txt", "r", stdin);
-  freopen("output.txt", "w", stdout);
-  freopen("error.txt", "w", stderr);
-#endif
+//Memoization
+//TC - O(n)
+//SC - O(n) for the stack space + O(n) for the dp array
 
-  fastio();
+#include<bits/stdc++.h>
+using namespace std;
 
-  int n;
-  cin>>n;
+int maxPoints(int a, int n, vector<vector<int>>& v, vector<vector<int>>& dp){
+	
+	if(n<0){
+		return 0;
+	}
+	
+	if(dp[n][a] != -1){
+		return dp[n][a];
+	}
+	
+	int best = 0;
+	
+	for(int i = 0; i < 3; i ++){
+		if(i != a){
+			best = max(best, maxPoints(i, n - 1, v, dp) + v[n][i]);
+		}
+	}
+	
+	return dp[n][a] = best;
+}
 
-  vector<vector<int>> a(n, vector<int> (3));
+int main(){
+	
+	int n;
+	cin>>n;
+	
+	vector<vector<int>> v(n, vector<int> (3));
+	
+	for(int i = 0; i < n; i ++){
+		for(int j = 0; j < 3; j ++){
+			cin>>v[i][j];
+		}
+	}
+	
+	vector<vector<int>> dp(n, vector<int> (4, -1));
+	
+	cout<<maxPoints(3, n - 1, v, dp);
+	
+	return 0;
+}
 
-  for(int i=0;i<n;i++){
-    for(int j=0;j<3;j++){
-        cin>>a[i][j];
-    }
-  }
 
-  vector<vector<int>> dp(n, vector<int> (4,-1));
+//Tabulation
+//TC - O(n)
+//SC - O(n) for the dp array
 
-  cout<<maxPoints(n-1,3,a,dp);
+#include<bits/stdc++.h>
+using namespace std;
 
-  return 0;
+int main(){
+	
+	int n;
+	cin>>n;
+	
+	vector<vector<int>> v(n, vector<int> (3));
+	
+	for(int i = 0; i < n; i ++){
+		for(int j = 0; j < 3; j ++){
+			cin>>v[i][j];
+		}
+	}
+	
+	vector<vector<int>> dp(n, vector<int> (4, 0));
+	for(int i = 0; i <= 3; i ++){
+		for(int j = 0; j < 3; j ++){
+			if(i != j){
+				dp[0][i] = max(dp[0][i], v[0][j]);
+			}
+		}
+	}
+	
+	for(int day = 1; day < n; day ++){
+		for(int last = 0; last <= 3; last ++){
+			for(int task = 0; task < 3; task ++){
+				if(task != last){
+					int curr = v[day][task] + dp[day - 1][task];
+					dp[day][last] = max(dp[day][last], curr);
+				}
+			}
+		}
+	}
+	
+	cout<<dp[n-1][3];
+	
+	return 0;
+}
+
+
+//Space Optimization
+//TC - O(n)
+//SC - O(1)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	
+	int n;
+	cin>>n;
+	
+	vector<vector<int>> v(n, vector<int> (3));
+	
+	for(int i = 0; i < n; i ++){
+		for(int j = 0; j < 3; j ++){
+			cin>>v[i][j];
+		}
+	}
+	
+	vector<int> prev(4, 0);
+	for(int i = 0; i <= 3; i ++){
+		for(int j = 0; j < 3; j ++){
+			if(i != j){
+				prev[i] = max(prev[i], v[0][j]);
+			}
+		}
+	}
+	
+	for(int day = 1; day < n; day ++){
+		vector<int> temp(4, 0);
+		for(int last = 0; last <= 3; last ++){
+			for(int task = 0; task < 3; task ++){
+				if(task != last){
+					int curr = v[day][task] + prev[task];
+					temp[last] = max(temp[last], curr);
+				}
+			}
+		}
+		prev = temp;
+	}
+	
+	cout<<prev[3];
+	
+	return 0;
 }
