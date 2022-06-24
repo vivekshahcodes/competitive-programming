0d3b8660c106e8deb9094e697222a601c9ad55b9
Vivek Shah <vivekshahcodes@gmail.com>
2022-06-24 17:23:45 +0530

    Update B - Frog 2.cpp

--- AtCoder/Educational DP Contest/B - Frog 2.cpp
+++ AtCoder/Educational DP Contest/B - Frog 2.cpp
@@ -1,71 +1,131 @@
+//Recursion
+//TC - O(k^n)
+//SC - O(n)
+
 #include<bits/stdc++.h>
 using namespace std;
 
-#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
-#define endl "\n"
+typedef long long ll;
 
-#ifndef ONLINE_JUDGE
-#define debug(x) cerr << #x<<" "; _print(x); cerr<< "\n";
-#else
-#define debug(x)
-#endif
+ll minCost(ll n, vector<ll>& v, int k){
+	
+	if(n==0){
+		return 0;
+	}
+	
+	ll ans = 1e18;
+	
+	for(ll i=1;i<=k;i++){
+		if(n-i>=0){
+			ans = min(ans, minCost(n-i,v,k) + abs(v[n] - v[n-i]));
+		}
+	}
+	
+	return ans;
+}
 
-template<class T> void _print(T a) { cerr << a; }
-template<class T, class V> void _print(pair<T,V> p){ _print('{'); _print(p.first); _print(','); _print(p.second); _print('}'); }
-template<class T> void _print(vector<T> v) { cerr << "[ "; for (T i : v) { _print(i); cerr << " "; } cerr << "]"; }
-template<class T> void _print(vector<vector<T>> v) { cerr << "\n"; for (auto i : v) { cerr << "[ "; _print(i); cerr << "]\n"; } }
-template<class T> void _print(set<T> s) { cerr << "[ "; for (T i : s) { _print(i); cerr << " "; } cerr << "]"; }
-template<class T, class V> void _print(map<T, V> m) { cerr << "\n"; for (auto i : m) { _print(i.first); cerr << " "; _print(i.second); cerr << "\n"; } }
+int main(){
+	
+	ll n,k;
+	cin>>n>>k;
+	
+	vector<ll> v(n);
+	for(ll i=0;i<n;i++){
+		cin>>v[i];
+	}
+	
+	cout<<minCost(n-1,v,k);
+	
+	return 0;
+}
 
+
+//Memoization
+//TC - O(n*k)
+//SC - O(n) for the stack space + O(n) for the dp array
+
+#include<bits/stdc++.h>
+using namespace std;
 
 typedef long long ll;
 
-/*------------------------------------------------------------------------------------------------------------------------------------------------------*/
-
-int minCost(vector<int> &a, int n, int k, vector<int>& dp){
-
-    if(n==0){
-        return 0;
-    }
-
-    if(dp[n]!=-1){
-        return dp[n];
-    }
-
-    int ans = INT_MAX;
-
-    for(int i=1;i<=k;i++){
-        if(n-i>=0){
-            ans = min(ans,minCost(a,n-i,k,dp)+abs(a[n-i]-a[n]));
-        }
-    }
-
-    return dp[n] = ans;
+ll minCost(ll n, vector<ll>& v, int k, vector<ll>& dp){
+	
+	if(n==0){
+		return 0;
+	}
+	
+	if(dp[n]!=-1){
+		return dp[n];
+	}
+	
+	ll ans = 1e18;
+	
+	for(ll i=1;i<=k;i++){
+		if(n-i>=0){
+			ans = min(ans, minCost(n-i,v,k,dp) + abs(v[n] - v[n-i]));
+		}
+	}
+	
+	return dp[n] = ans;
 }
 
-/*------------------------------------------------------------------------------------------------------------------------------------------------------*/
-
-int main() {
-
-#ifndef ONLINE_JUDGE
-  freopen("input.txt", "r", stdin);
-  freopen("output.txt", "w", stdout);
-  freopen("error.txt", "w", stderr);
-#endif
-
-  fastio();
-
-  int n,k;
-  cin>>n>>k;
-
-  vector<int> a(n);
-  for(int i=0;i<n;i++){
-    cin>>a[i];
-  }
-
-  vector<int> dp(n,-1);
-
-  cout<<minCost(a,n-1,k,dp);
-
-  return 0;
+int main(){
+	
+	ll n,k;
+	cin>>n>>k;
+	
+	vector<ll> v(n);
+	for(ll i=0;i<n;i++){
+		cin>>v[i];
+	}
+	
+	vector<ll> dp(n,-1);
+	
+	cout<<minCost(n-1,v,k,dp);
+	
+	return 0;
 }
+
+
+//Tabulation
+//TC - O(n*k)
+//SC - O(n) for the dp array
+
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+	
+	ll n,k;
+	cin>>n>>k;
+	
+	vector<ll> v(n);
+	for(ll i=0;i<n;i++){
+		cin>>v[i];
+	}
+	
+	vector<ll> dp(n);
+	dp[0] = 0;
+	
+	for(ll i=1;i<n;i++){
+		dp[i] = 1e18;
+		for(ll j=1;j<=k;j++){
+			if(i-j>=0){
+				dp[i] = min(dp[i], dp[i-j] + abs(v[i]-v[i-j]));
+			}
+		}
+	}
+	
+	cout<<dp[n-1];
+	
+	return 0;
+}
+
+
+//Space Optimization
+//TC - O(n*k)
+//SC - O(k) for the dp array
+//Note: This optimization is not worth it, if k tends to n.
