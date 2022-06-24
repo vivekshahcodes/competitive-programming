2f75b1d831e3966ce6802996f8f68f89d68ab3f8
Vivek Shah <vivekshahcodes@gmail.com>
2022-06-24 15:28:08 +0530

    Update A - Frog 1.cpp

--- AtCoder/Educational DP Contest/A - Frog 1.cpp
+++ AtCoder/Educational DP Contest/A - Frog 1.cpp
@@ -1,68 +1,159 @@
+//Recursion
+//TC - O(2^n), more precisely, O(1.618^n)
+//SC - O(n) for the stack space
+
 #include<bits/stdc++.h>
 using namespace std;
- 
-#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
-#define endl "\n"
- 
-#ifndef ONLINE_JUDGE
-#define debug(x) cerr << #x<<" "; _print(x); cerr<< "\n";
-#else
-#define debug(x)
-#endif
- 
-template<class T> void _print(T a) { cerr << a; }
-template<class T, class V> void _print(pair<T,V> p){ _print('{'); _print(p.first); _print(','); _print(p.second); _print('}'); }
-template<class T> void _print(vector<T> v) { cerr << "[ "; for (T i : v) { _print(i); cerr << " "; } cerr << "]"; }
-template<class T> void _print(vector<vector<T>> v) { cerr << "\n"; for (auto i : v) { cerr << "[ "; _print(i); cerr << "]\n"; } }
-template<class T> void _print(set<T> s) { cerr << "[ "; for (T i : s) { _print(i); cerr << " "; } cerr << "]"; }
-template<class T, class V> void _print(map<T, V> m) { cerr << "\n"; for (auto i : m) { _print(i.first); cerr << " "; _print(i.second); cerr << "\n"; } }
- 
- 
+
 typedef long long ll;
- 
-/*------------------------------------------------------------------------------------------------------------------------------------------------------*/
- 
-int minCost(vector<int>& a, int n, vector<int>& dp){
- 
-    if(n==0){
-        return 0;
-    }
- 
-    if(dp[n]!=-1){
-        return dp[n];
-    }
- 
-    int first = minCost(a,n-1,dp)+abs(a[n]-a[n-1]);
-    int second = INT_MAX;
-    if(n-2>=0){
-        second = minCost(a,n-2,dp)+abs(a[n]-a[n-2]);
-    }
- 
-    return dp[n] = min(first,second);
+
+ll minCost(ll n, vector<ll>& v){
+	
+	if(n==0){
+		return 0;
+	}
+	
+	ll a = minCost(n-1,v) + abs(v[n]-v[n-1]);
+	ll b = 1e18;
+	if(n-2>=0){
+		b = minCost(n-2,v) + abs(v[n]-v[n-2]);
+	}
+	
+	return min(a,b);
 }
- 
-/*------------------------------------------------------------------------------------------------------------------------------------------------------*/
- 
-int main() {
- 
-#ifndef ONLINE_JUDGE
-  freopen("input.txt", "r", stdin);
-  freopen("output.txt", "w", stdout);
-  freopen("error.txt", "w", stderr);
-#endif
- 
-  fastio();
- 
-  int n;
-  cin>>n;
- 
-  vector<int> a(n);
-  for(int i=0;i<n;i++){
-    cin>>a[i];
-  }
- 
-  vector<int> dp(n,-1);
-  cout<<minCost(a,n-1,dp);
- 
-  return 0;
+
+int main(){
+	
+	ll n;
+	cin>>n;
+	
+	vector<ll> v(n);
+	for(ll i=0;i<n;i++){
+		cin>>v[i];
+	}
+	
+	cout<<minCost(n-1,v);
+	
+	return 0;
+}
+
+
+//Memoization
+//TC - O(n)
+//SC - O(n) for the stack space + O(n) for the dp array
+
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+ll minCost(ll n, vector<ll>& v, vector<ll>& dp){
+	
+	if(n==0){
+		return 0;
+	}
+	
+	if(dp[n]!=-1){
+		return dp[n];
+	}
+	
+	ll a = minCost(n-1,v,dp) + abs(v[n]-v[n-1]);
+	ll b = 1e18;
+	if(n-2>=0){
+		b = minCost(n-2,v,dp) + abs(v[n]-v[n-2]);
+	}
+	
+	return dp[n] = min(a,b);
+}
+
+int main(){
+	
+	ll n;
+	cin>>n;
+	
+	vector<ll> v(n);
+	for(ll i=0;i<n;i++){
+		cin>>v[i];
+	}
+	
+	vector<ll> dp(n,-1);
+	
+	cout<<minCost(n-1,v,dp);
+	
+	return 0;
+}
+
+
+//Tabulation
+//TC - O(n)
+//SC - O(n) for the dp array
+
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+	
+	ll n;
+	cin>>n;
+	
+	vector<ll> v(n);
+	for(ll i=0;i<n;i++){
+		cin>>v[i];
+	}
+	
+	vector<ll> dp(n);
+	dp[0] = 0;
+	
+	for(ll i=1;i<n;i++){
+		ll a = dp[i-1] + abs(v[i]-v[i-1]);
+		ll b = 1e18;
+		if(i>1){
+			b = dp[i-2] + abs(v[i]-v[i-2]);
+		}
+		dp[i] = min(a,b);
+	}
+	
+	cout<<dp[n-1];
+	
+	return 0;
+}
+
+
+//Space Optimization
+//TC - O(n)
+//SC - O(1)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+	
+	ll n;
+	cin>>n;
+	
+	vector<ll> v(n);
+	for(ll i=0;i<n;i++){
+		cin>>v[i];
+	}
+	
+	ll a = 0, b = 0, c;
+	
+	for(ll i=1;i<n;i++){
+		ll first = b + abs(v[i]-v[i-1]);
+		ll second = 1e18;
+		if(i>1){
+			second= a + abs(v[i]-v[i-2]);
+		}
+		c = min(first,second);
+		a = b;
+		b = c;
+	}
+	
+	cout<<c;
+	
+	return 0;
 }
